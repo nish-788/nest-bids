44ea08996a91ef62c08d400c9175e390134b6170
Ashwani <ashwanibhagat2002@gmail.com>
2023-10-26 02:13:29 +0530

    update user functionality in profile page

--- estate_project/src/pages/Profile.jsx
+++ estate_project/src/pages/Profile.jsx
@@ -1,17 +1,20 @@
 import React, { useState,useEffect } from 'react';
 import { useRef } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector,useDispatch } from 'react-redux';
 import { getDownloadURL, getStorage,ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from '../firebase';
+import { updateUserStart , updateUserSuccess, updateUserFailure } from '../redux/user/userSlice';
 
 
 function Profile() {
   const fileRef=useRef(null);
-  const {currentUser}=useSelector((state)=>state.user);
+  const {currentUser,loading,error}=useSelector((state)=>state.user);
   const [file,setFile]=useState(undefined);
   const [filePerc,setFilePerc]=useState(0);
   const[fileUploadError,setFileUploadError]=useState(false);
   const [formData,setFormData]=useState({});
+  const [updateSuccess,setUpdateSuccess]=useState(false);
+  const dispatch= useDispatch();
 
 
  // console.log(formData)
@@ -50,12 +53,41 @@ function Profile() {
     );
   };
 
+  const handleChange=(e)=>{
+    setFormData({ ...formData, [e.target.id] : e.target.value });
+  };
+
+  const handleSubmit= async(e)=>{
+    e.preventDefault();
+    try{
+    dispatch(updateUserStart());
+    const res= await fetch(`/api/user/update/${currentUser._id}`,
+    {
+      method:'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body:JSON.stringify(formData),
+    });
+    const data= await res.json();
+      if(data.success === false){
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    }
+    catch(error){
+    dispatch(updateUserFailure(error.message));
+    }
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>
         Profile
       </h1>
-      <form className='flex flex-col gap-4'>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input onChange={(e)=>setFile(e.target.files[0])} 
          type='file' ref={fileRef} hidden accept='image/*' />
 
@@ -83,15 +115,30 @@ function Profile() {
       }
         </p>
 
-        <input type="text" placeholder='username' id='username' className='border p-3
-        rounded-lg' />
-         <input type="text" placeholder='email' id='email' className='border p-3
-        rounded-lg' />
-         <input type="text" placeholder='password' id='password' className='border p-3
-        rounded-lg' />
-        <button className='bg-slate-700 text-white rounded-lg
+        <input
+         type="text"
+          placeholder='username'
+          defaultValue={currentUser.username}
+          id='username'
+           className='border p-3 rounded-lg' 
+           onChange={handleChange}
+           />
+         <input type="text"
+          placeholder='email'
+          defaultValue={currentUser.email}
+           id='email'
+            className='border p-3 rounded-lg'
+            onChange={handleChange}
+            />
+         <input type="password" 
+         placeholder='password'
+          id='password'
+           className='border p-3 rounded-lg'
+           onChange={handleChange}
+           />
+        <button disabled={loading} className='bg-slate-700 text-white rounded-lg
         p-3 uppercase hover:opacity-95 disabled:obacity-80'>
-          Update
+          {loading ? 'Loading...' : 'Update'}
         </button>
       </form>
 
@@ -99,6 +146,10 @@ function Profile() {
      <span className='text-red-700 cursor-pointer hover:opacity-75'>Delete account</span>
      <span className='text-red-700 cursor-pointer hover:opacity-75'>Sign out</span>
       </div>
+
+      <p className='text-red-700 mt-5'>{error ? error : ""} </p>
+
+      <p className='text-green-700 mt-5'>{updateSuccess ? 'User updated successfully!':''}</p>
     </div>
   )
 }
